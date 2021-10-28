5b3ebce92abbba5ab6e490d798a655f6ab375c55
sienning <sienning@hotmail.fr>
2021-10-28 15:08:38 +0200

    inscription + connexion

--- routes/connexion.js
+++ routes/connexion.js
@@ -5,35 +5,34 @@ const bcrypt = require('bcryptjs');
 const jsontoken = require('jsonwebtoken')
 
 //Discord connection tuto
-router.get('/', passport.authenticate('discord'), (res, req) => {
-  console.log(res);
-});
+router.get('/', passport.authenticate('discord'));
 router.get('/redirect', passport.authenticate('discord', {
   failureRedirect: '/forbidden',
   successRedirect: '/profil'
 }));
 
-router.get('/login', (req, res) => {
+router.post('/login', (req, res) => {
   userModel.findOne({ email: req.body.email })
     .then(user => {
+      console.log(user);
       if (!user) {
-        return res.status(401).json({ error: 'pas trouvé' })
+        return res.send({status : "error", message : "Cet utilisateur n'existe pas ! Verifiez votre adresse mail."});
       }
       bcrypt.compare(req.body.password, user.password)
         .then(ok => {
           if (!ok) {
-            return res.status((401).json({ error: 'incorect mdp' }))
+            return res.send({status : "error", message : "Mot de passe incorrect."});
           }
           res.status(200).json({
+            userInfos: user,
             userId: user._id,
             token: jsontoken.sign(
-              { userId: user._id }, 'cryptage', { expiresIn: '48h' }
+              { userId: user._id }, 'cryptage', { expiresIn: '24h' }
             )
           })
         })
         .catch(error => res.status(500).json({ error }))
     })
-
 })
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const auth = require('../controller/auth');
+const { userModel } = require('./../models/user');
+const mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/see-user/:id', auth, function (req, res, next) {
+  let id = mongoose.Types.ObjectId(req.params.id);
+  userModel.findById(id)
+    .then(user => { res.send(user) })
+    .catch(error => { res.status(500).json({ error }) })
 });
 
 module.exports = router;
